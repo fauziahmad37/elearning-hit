9aec63816bc3efa6cacdac96ec77c35ba5fd8174
fauziahmad37 <fauzi.enginer@gmail.com>
2023-08-16 17:31:45 +0700

    first commit

---
+++ application/views/footer.php
@@ -0,0 +1,21 @@
+		</main>
+
+		<footer class="site-footer section-padding">
+            <div class="container">
+                <div class="row">
+
+                    
+                </div>
+            </div>
+        </footer>
+
+
+        <!-- JAVASCRIPT FILES -->
+        <script src="<?=base_url('assets/js/jquery.min.js')?>"></script>
+        <script src="<?=base_url('assets/js/bootstrap.bundle.min.js')?>"></script>
+        <script src="<?=base_url('assets/js/jquery.sticky.js')?>"></script>
+        <script src="<?=base_url('assets/js/click-scroll.js')?>"></script>
+        <script src="<?=base_url('assets/js/custom.js')?>"></script>
+
+    </body>
+</html>
