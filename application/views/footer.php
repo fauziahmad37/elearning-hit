839d1798e2d3b55f0c6371f02cec7b66e17e51a4
fauziahmad37 <fauzi.enginer@gmail.com>
2023-08-18 15:01:29 +0700

    no change

--- application/views/footer.php
+++ application/views/footer.php
@@ -11,8 +11,12 @@
 
 
         <!-- JAVASCRIPT FILES -->
-        <script src="<?=base_url('assets/js/jquery.min.js')?>"></script>
-        <script src="<?=base_url('assets/js/bootstrap.bundle.min.js')?>"></script>
+		
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+		
+
+        <!-- <script src="<?=base_url('assets/js/jquery.min.js')?>"></script> -->
+        <!-- <script src="<?=base_url('assets/js/bootstrap.bundle.min.js')?>"></script> -->
         <script src="<?=base_url('assets/js/jquery.sticky.js')?>"></script>
         <script src="<?=base_url('assets/js/click-scroll.js')?>"></script>
         <script src="<?=base_url('assets/js/custom.js')?>"></script>
