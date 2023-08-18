6320e1763429ccce4400a91e54ef0178c52d2cdc
fauziahmad37 <fauzi.enginer@gmail.com>
2023-08-18 15:02:08 +0700

    create controller Home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -21,7 +21,7 @@ class Home extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('header');
-		$this->load->view('index');
+		$this->load->view('home/index');
 		$this->load->view('footer');
 	}
 }
